2b605d3954708a18c8366cb186fb3ed316543918
robertkleinschuster <robert.kleinschuster@niceshops.com>
2020-07-17 20:29:51 +0200

    Setup Vue.js

---
+++ App.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+<div>
+
+</div>
+</template>
+
+<script lang="ts">
+  import Vue from "vue";
+
+  export default Vue.extend({
+    data() {
+      return {
+        bundler: "Parcel"
+      };
+    }
+  });
+</script>
+
+<style lang="scss" scoped>
+  .container {
+    color: green;
+  }
+</style>
+
